a6a7c105db27e26954e9ba3e54dcb7bc7241f380
macro6461 <mattcroak718@gmail.com>
2017-10-27 09:39:36 -0400

    Done.

--- src/createStore.js
+++ src/createStore.js
@@ -1,5 +1,23 @@
+import candyReducer from './reducers/candyReducer'
+import countReducer from './reducers/countReducer'
+
 export default function createStore(reducer) {
+
+  let state;
   // add your code here
+
+  function dispatch(action){
+    state = reducer(state, action)
+  }
+
+   function getState(){
+     return state
+   }
+
+   dispatch({type: "@@INIT"})
+
+   return { dispatch, getState }
+
 }
 
 function render() {
